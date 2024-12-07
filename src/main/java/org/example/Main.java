4c643a90c164fec16dcb12fbbf59fb4be1f0fb59
WiT1t <witkowal50@gmail.com>
2024-12-07 18:15:54 +0100

    add Client/Client, Client/RunClient add Server/Server, Server/RunServer, Server/UserThread

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,9 +5,5 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
     }
 }
