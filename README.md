17790304df650dcbc38943f07aec6a11e1481375
michalpielka <mpielka726@gmail.com>
2025-01-12 17:50:38 +0100

    try remove files from .gitignore

--- README.md
+++
@@ -1,2 +0,0 @@
-To initialize the server, you should run Server/RunServer class.
-To initialize a client, you should run Client/RunClient class.
--- src/main/java/org/example/Server/Commands/Command.java
+++
@@ -1,7 +0,0 @@
-package org.example.Server.Commands;
-
-import org.example.Server.UserThread;
-
-public interface Command {
-    void execute(UserThread user, String[] args);
-}
